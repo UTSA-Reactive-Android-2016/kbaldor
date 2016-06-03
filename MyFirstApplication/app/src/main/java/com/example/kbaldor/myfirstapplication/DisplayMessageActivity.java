8afb9b1b169436428cf227a0f42802aefb0539da
Kevin Baldor <kevin.baldor@gmail.com>
2016-06-02 20:29:54 -0500

    It's broke, but I love it anyway.

--- MyFirstApplication/app/src/main/java/com/example/kbaldor/myfirstapplication/DisplayMessageActivity.java
+++ MyFirstApplication/app/src/main/java/com/example/kbaldor/myfirstapplication/DisplayMessageActivity.java
@@ -1,5 +1,6 @@
 package com.example.kbaldor.myfirstapplication;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
@@ -20,5 +21,42 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
+        System.out.println("DisplayMessageActivity: onCreate called with bundle "+savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("DisplayMessageActivity: onStart called.");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("DisplayMessageActivity: onRestart called.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("DisplayMessageActivity: onPause called.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("DisplayMessageActivity: onStop *************************** called.");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        System.out.println("DisplayMessageActivity: onSaveInstanceState called.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("DisplayMessageActivity: onDestroy called");
     }
 }
